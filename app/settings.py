9e319ad222489b6973627470312e86fa04b7d6c1
Ivanov Sergey <djapy@yandex.ru>
2017-12-15 18:23:08 +0300

    Fix troubles with static not found

--- app/settings.py
+++ app/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_DIRS = (
     os.path.join(BASE_DIR, "media"),
     '/media/'
