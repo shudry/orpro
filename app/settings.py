f87e3f168b9cca5f14fff040acfdb753d98bc9cd
Ivanov Sergey <djapy@yandex.ru>
2017-12-27 15:29:18 +0300

    Add dj-database-url v.3

--- app/settings.py
+++ app/settings.py
@@ -85,24 +85,25 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 import dj_database_url
+
+
+DATABASES = {
+    'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': '**************',
+                'USER': '**************',
+                'PASSWORD': '*****************************************',
+                'HOST': 'ec2-54-247-187-134.eu-west-1.compute.amazonaws.com',
+                'PORT': '5432',
+        #'OPTIONS': {
+        #    'init_command': 'SET innodb_strict_mode=1',
+        #    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #},
+    }
+}
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
-# DATABASES = {
-#     'default': {
-#                 'ENGINE': 'django.db.backends.postgresql',
-#                 'NAME': 'd49pql4r4es94b',
-#                 'USER': 'zszykjlgczqtps',
-#                 'PASSWORD': '7de6e7e39c83d4f7227a321f7aa2632fef95cc89fb4168fb7698b9579f0712b8',
-#                 'HOST': 'ec2-54-247-187-134.eu-west-1.compute.amazonaws.com',
-#                 'PORT': '5432',
-#         #'OPTIONS': {
-#         #    'init_command': 'SET innodb_strict_mode=1',
-#         #    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         #},
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
