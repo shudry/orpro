cbb7644d89ba45eb97b3767aac2dd15a5b1b25c1
Dmytry Shumilo <shumilo.dmytry@gmail.com>
2018-04-08 09:58:08 -0400

    (New-frontend 4) Completion of page changes displayed to ordinary users

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^', include('pages.urls')),
     url(r'^captcha/', include('captcha.urls')),
+    url(r'^', include('pages.urls')),
 ]
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
