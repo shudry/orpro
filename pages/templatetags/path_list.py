fc7ea38d3008eb73e65f4764dabcb4a4ad2fe1ef
shudry <shumilo.dmytry@gmail.com>
2018-04-23 21:36:12 +0300

    Fixed displaying the first four tags, as well as an attempt to display icons in the editor

--- pages/templatetags/path_list.py
+++ pages/templatetags/path_list.py
@@ -1,3 +1,4 @@
+import random
 from django import template
 
 register = template.Library()
@@ -7,3 +8,9 @@ register = template.Library()
 def return_path_list(value, index='None'):
 	split_list = value.split('/')[1:]
 	return split_list if type(index) != int else split_list[index]
+
+
+@register.filter(name='random_sort')
+def random_sort(tags_list):
+	''' Blends the tag list for the cloud '''
+	return sorted(tags_list, key=lambda x: random.random())
