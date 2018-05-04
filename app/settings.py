340fdf3b1d993f2575aba167275bc8004c1fbf63
shudry <shumilo.dmytry@gmail.com>
2018-05-04 22:28:01 +0300

    Add context_processors

--- app/settings.py
+++ app/settings.py
@@ -78,6 +78,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #Custom processors
+				'pages.utils.context_processors.orpro_data',
             ],
         },
     },
